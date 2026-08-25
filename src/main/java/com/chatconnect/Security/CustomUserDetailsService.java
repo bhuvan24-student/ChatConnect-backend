52d617159e0958a11e8e15f5e176e45433f6ab4c
Bhuvan teja <balabhuvanteja@gmail.com>
2026-08-25 23:36:29 +0530

    Updated authentication

--- src/main/java/com/chatconnect/Security/CustomUserDetailsService.java
+++ src/main/java/com/chatconnect/Security/CustomUserDetailsService.java
@@ -3,10 +3,11 @@ package com.chatconnect.Security;
 import com.chatconnect.Users.User;
 import com.chatconnect.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
+import org.springframework.security.core.userdetails.User;
 import java.util.Optional;
 
 public class CustomUserDetailsService implements UserDetailsService {
@@ -21,6 +22,9 @@ public class CustomUserDetailsService implements UserDetailsService {
          if(!user.isPresent()){
              throw  new UsernameNotFoundException("User not found");
          }
-        return null;
+        org.springframework.security.core.userdetails.User
+                .withUsername(user.get().getEmail())
+                .password(user.get().getPassword())
+                .authorities(new SimpleGrantedAuthority("ROLE_"+user.get().getRole())).build();
     }
 }
--- src/main/java/com/chatconnect/Users/User.java
+++ src/main/java/com/chatconnect/Users/User.java
@@ -18,6 +18,8 @@ public class User {
     private String email;
     @Column
     private String password;
+    @Column
+    private String role;
 
     public long getId() {
         return id;
@@ -49,5 +51,12 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
